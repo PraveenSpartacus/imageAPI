fb1d5a27e8a5298c54fc01a31a08f09d052f5190
Praveen Vipparthi <praveen@Praveens-MacBook-Air.local>
2023-02-10 14:30:24 +0530

    #2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_file
+import random,os
   
 app = Flask(__name__) #creating the Flask class object   
  
@@ -8,7 +9,9 @@ def home():
 
 @app.route('/image')
 def send_image():
-    imagePath = './static/IMG/img.png'
+    arr = os.listdir('./static/IMG')
+    image_file = arr[random.randint(0,len(arr)-1)]
+    imagePath = './static/IMG/' + image_file
     return send_file(imagePath, mimetype='image/gif')
   
 if __name__ =='__main__':  
